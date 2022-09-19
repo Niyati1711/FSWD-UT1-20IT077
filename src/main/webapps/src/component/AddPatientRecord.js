fea34a978610ecc315ed0befbb8d707b32119cec
Niyati Pandya <88589883+Niyati1711@users.noreply.github.com>
2022-09-19 12:41:50 +0530

    Update AddPatientRecord.js

--- src/main/webapps/src/component/AddPatientRecord.js
+++ src/main/webapps/src/component/AddPatientRecord.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-
 import { Container } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import axios from 'axios'
+
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
